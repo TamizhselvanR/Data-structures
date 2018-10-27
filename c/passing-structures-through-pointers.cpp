aa2f8bd12e6e470319bb052ece7a772d2a9a4f6c
Tamizh selvan <rvtamizhselvan@gmail.com>
2018-10-27 23:17:10 +0530

    Added cpp file of problem in structures

--- c/passing-structures-through-pointers.cpp
+++ c/passing-structures-through-pointers.cpp
@@ -1,23 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
-int main()
-{
-
 struct details
 {
 	int roll;
-	
 };
 struct details *ptr[60];
-for(int i=0;i<4;i++)
-{
-ptr[i] = (struct details *)malloc(sizeof(struct details));
-printf("enter the %d",i);
-scanf("%d\n",&ptr[i]->roll);
-}
-for(int i=0;i<4;i++)
+int main()
+{	
+	for(int i=0;i<4;i++)
 	{
-	printf("the no is %d\n",ptr[i]->roll);
+		ptr[i] = (struct details *)malloc(sizeof(struct details));
+		printf("enter the %d th no\n",i);
+		scanf("%d",&ptr[i]->roll);
+	}
+	for(int i=0;i<4;i++)
+	{
+		printf("the no is %d\n",ptr[i]->roll);
 	}	
 }
